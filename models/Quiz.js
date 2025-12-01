1ec4748001c0ce85ca5e1eb2940aaa0d19df8028
Asif Ahammed <asifahammed359@gmail.com>
2025-12-01 23:50:22 +0600

    update quiz modal

--- models/Quiz.js
+++ models/Quiz.js
@@ -1,6 +1,25 @@
 // models/Quiz.js
 const mongoose = require("mongoose");
 
+const questionSchema = new mongoose.Schema(
+  {
+    questionText: {
+      type: String,
+      required: true,
+    },
+    options: {
+      type: [String],
+      required: true,
+      validate: (v) => Array.isArray(v) && v.length >= 2,
+    },
+    correctOptionIndex: {
+      type: Number,
+      required: true,
+    },
+  },
+  { _id: false }
+);
+
 const quizSchema = new mongoose.Schema(
   {
     course: {
@@ -19,6 +38,7 @@ const quizSchema = new mongoose.Schema(
     questions: {
       type: [questionSchema],
       required: true,
+      validate: (v) => Array.isArray(v) && v.length > 0,
     },
   },
   {
